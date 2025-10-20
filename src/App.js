89cb8f6d2281152b79e1f3f501fad5a4be0ddb65
Erangamadhushan <erangamadhushann956@gmail.com>
2025-10-20 20:23:07 +0530

    Add Features component with health feature highlights and icons

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Header from './components/Header/Header';
 import { HeroSection } from './components/HeroSection/HeroSection';
+import { Features } from './components/features/Features';
 import { Footer } from './components/Footer/Footer';
 
 
@@ -8,9 +9,10 @@ function App() {
   return (
     <>
       <Header />
-      <main>
+      <main className="bg-black text-white">
         {/* Main content goes here */}
         <HeroSection />
+        <Features />
       </main>
       <Footer />
     </>
