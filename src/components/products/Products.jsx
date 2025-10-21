794253dc4db8461eb0c7ac94f19cb723ea863326
Erangamadhushan <erangamadhushann956@gmail.com>
2025-10-21 20:34:55 +0530

    Add product data and update Products component to dynamically render product listings

--- src/components/products/Products.jsx
+++ src/components/products/Products.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { products } from '../../data/product.data'
 
 export const Products = () => {
   return (
@@ -18,48 +19,24 @@ export const Products = () => {
             </div>
             <div>
                 <div className="grid grid-cols-1 md:grid-cols-3 gap-5">
-                    <div className="group">
-                        <div className="group-hover:shadow-lg group-hover:scale-105 transition-all duration-300 ease-in-out">
-                            <div>
-                                <img src="product1.png" alt="digital-watch-type-1" className="size-52 group-hover:scale-110 transition-all duration-300 ease-in-out  object-cover mx-auto"/>
+                    {
+                        products.map((product) => (
+                            <div className="group">
+                                <div className="group-hover:shadow-lg group-hover:scale-105 transition-all duration-300 ease-in-out">
+                                    <div>
+                                        <img src={product.img} alt={product.name} className="size-52 group-hover:scale-110 transition-all duration-300 ease-in-out  object-cover mx-auto"/>
+                                    </div>
+                                    <div className="my-5">
+                                        <h2 className="text-center text-lg font-semibold">{product.name}</h2>
+                                        <p className="text-center text-lime-500">${product.price}</p>
+                                        <p className="text-center p-5">
+                                            {product.desc}
+                                        </p>
+                                    </div>
+                                </div>
                             </div>
-                            <div className="my-5">
-                                <h2 className="text-center text-lg font-semibold">Alpine Loop</h2>
-                                <p className="text-center text-lime-500">$1,900</p>
-                                <p className="text-center p-5">
-                                    Customized for your best performance, this cutting edge Storm connected watch pushes the boundaries of technology.
-                                </p>
-                            </div>
-                        </div>
-                    </div>
-                    <div className="group">
-                        <div className="group-hover:shadow-lg group-hover:scale-105 transition-all duration-300 ease-in-out">
-                            <div>
-                                <img src="product2.png" alt="digital-watch-type-2" className="size-52 group-hover:scale-110 transition-all duration-300 ease-in-out  object-cover mx-auto"/>
-                            </div>
-                            <div className="my-5">
-                                <h2 className="text-center text-lg font-semibold">Ocean Band</h2>
-                                <p className="text-center text-lime-500">$1,999</p>
-                                <p className="text-center p-5">
-                                    A new dimension in watchmaking, this cutting edge Storm Connected watch pushes the boundaries of technology.
-                                </p>
-                            </div>
-                        </div>
-                    </div>
-                    <div className="group">
-                        <div className="group-hover:shadow-lg group-hover:scale-105 transition-all duration-300 ease-in-out">
-                            <div>
-                                <img src="product3.png" alt="digital-watch-type-3" className="size-52 group-hover:scale-110 transition-all duration-300 ease-in-out  object-cover mx-auto"/>
-                            </div>
-                            <div className="my-5">
-                                <h2 className="text-center text-lg font-semibold">Trail Loop</h2>
-                                <p className="text-center text-lime-500">$2,075</p>
-                                <p className="text-center p-5">
-                                    Opening an exciting new dimension in digital watchmaking, this advanced Storm connected watch is as luxurious as ever.
-                                </p>
-                            </div>
-                        </div>
-                    </div>
+                        ))
+                    }
                 </div>
             </div>
         </div>
---
+++ src/data/product.data.js
@@ -0,0 +1,20 @@
+export const products = [
+    {
+        img: 'product1.png',
+        name: 'Alpine Loop',
+        price: '1,900',
+        desc: 'Customized for your best performance, this cutting edge Storm connected watch pushes the boundaries of technology.'
+    },
+    {
+        img: 'product2.png',
+        name: 'Ocean Band',
+        price: '1,999',
+        desc: 'A new dimension in watchmaking, this cutting edge Storm Connected watch pushes the boundaries of technology.'
+    },
+    {
+        img: 'product3.png',
+        name: 'Trail Loop',
+        price: '2,075',
+        desc: 'Opening an exciting new dimension in digital watchmaking, this advanced Storm connected watch is as luxurious as ever.'
+    }
+]
